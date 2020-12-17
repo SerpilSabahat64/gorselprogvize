a7fb36386215176a116161b944b61cb69625c397
SerpilSabahat64 <76158523+SerpilSabahat64@users.noreply.github.com>
2020-12-17 13:25:13 +0300

    Add files via upload

---
+++ 1soru.py
@@ -0,0 +1,12 @@
+﻿# Soru 1.
+# string func. kullanilmayacagi icin regular expression kullanildi.
+import re
+
+exp = '^[a-z0-9\._]+[@]\w+[.]\w{2,3}([.]\w{2})?$'
+def validateEMail(dogrulama):
+    if (re.search(exp, dogrulama)):
+    	print("---E-Posta adresi doğru---")
+    else: 
+    	print("---E-Posta adresi yanlış!---")
+
+validateEMail("serpilbasyigit.90@usak.edu.tr")
