d52f70029306d4b9490ae20f672693c0ed8fdeb6
SerpilSabahat64 <76158523+SerpilSabahat64@users.noreply.github.com>
2020-12-17 13:33:08 +0300

    Add files via upload

---
+++ 2soru.py
@@ -0,0 +1,12 @@
+﻿# Soru 2.
+# string func. kullanilmayacagi icin regular expression kullanildi.
+import re
+
+ornek = '^(http[s]?:\/\/)?([w]{3}\.)?[a-z0-9\._-]+[.]\w{2,3}([.]\w{2})?$'
+def validateURL(adres):
+    if (re.search(ornek, adres)):
+    	print("---URL adresi doğrulandı---")
+    else: 
+    	print("---URL hatalı---!")
+
+validateURL("www.google.com")
---
+++ 3soru.py
@@ -0,0 +1,10 @@
+﻿# Soru 3.
+metin = "Bugün günlerden Perşembe"
+aranan = "nle"
+bulunan = metin.find(aranan)
+if (bulunan > 0):
+	pre = metin[bulunan - 1]
+	post = metin[bulunan + len(aranan)]
+	print(pre + aranan + post)
+else:
+	print("Aranan değer bulunamadı!")
